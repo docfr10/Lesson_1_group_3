a8c9b51ac5661228e5d6d9a94928980215e3bc29
docfr10 <docfr10@yandex.ru>
2024-10-15 10:25:38 +0300

    Lesson 15 oct

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,7 @@ package org.example
 import kotlin.math.pow
 
 fun main() {
+    // ЗАНЯТИЕ 1
     // Вывод в консоль
     print("New text!\nAgain new text!\nHi!")
     println("Hello World!")
@@ -73,6 +74,151 @@ fun main() {
     println("Ввод через консоль")
     val input = readln().toInt()
     println(input)
-    val (q, w, t) = readln().split(" ")
+    val (q, w, t) = readln().split(" ").map { it.toInt() }
     println("$q, $w, $t")
+
+    // ЗАНЯТИЕ 2
+    // Условные операторы
+    // If-else
+    if (b1 > a2)
+        println(b1)
+    else if (b1 == a2) {
+        println("b1==a2")
+    } else {
+        println(a2)
+    }
+    // When
+    val day = 32
+    val dayName = when (day) {
+        1 -> "Пн"
+        2 -> "Вт"
+        3 -> "Ср"
+        4 -> "Чт"
+        5 -> "Пт"
+        6, 7 -> "Выходной день!"
+        in 8..31 -> "Выход за пределы календаря"
+        else -> "Некорректный день!"
+    }
+    println(dayName)
+    val max = when (b1 > a2) {
+        true -> b1
+        false -> a2
+    }
+    println(max)
+    // Циклы
+    // For
+    println("FOR")
+    for (i in 1..5) {
+        if (i == 4) continue
+        if (i == 5) break
+        println(i)
+    }
+    // Разворот диапозона с шагом 25
+    for (i in 100 downTo 1 step 25) {
+        println(i)
+    }
+    // Игнорирование последней границы
+    for (i in 1 until 10) {
+        println(i)
+    }
+    // While
+    println("WHILE")
+    var num = 5
+    while (num > 0) {
+        println(num)
+        num--
+    }
+    // Do-While
+    println("Do-While")
+    var num2 = 0
+    do {
+        println(num2)
+        num2++
+    } while (num2 < 5)
+    // Коллекции
+    // Массивы
+    println("ARRAY")
+    val array = arrayOf<Int>(1, 2, 3, 4, 5, 1)
+    val emptyArray = emptyArray<String>()
+    val specialArray1 = booleanArrayOf(true, false)
+    val specialArray2 = intArrayOf(1, 2, 6)
+    val specialArray3 = doubleArrayOf(5.9, 4.2)
+    array[3] = 10
+    println(array[3])
+    println(array.first())
+    println(array.last())
+    println("Array size =  ${array.size}")
+    println(array.indexOf(1))
+    println(array.contains(10))
+    // Многомерный массив
+    val arrayM = Array(5, { arrayOf(1, 2, 3, 4, 5) })
+    for (j in arrayM) {
+        for (i in j) {
+            println(i)
+        }
+    }
+    // Списки
+    // Неизменяемый
+    val list = listOf("afsad", " asfasfsa", "f")
+    // Изменяемый список
+    println("MUTABLE LIST")
+    val mutableList = mutableListOf("asf", "asf", "tasfaf", "sgeds")
+    mutableList.add("ihsaohd")
+    mutableList.add(0, "11212q")
+    mutableList.addAll(listOf("3123", "4124", "55"))
+
+    mutableList.forEach {
+        println(it)
+    }
+
+    // Подсписок
+    mutableList.subList(0, 3)
+
+    mutableList[2] = "dfafasfaf"
+
+    mutableList.remove("asf")
+    mutableList.removeAt(4)
+    mutableList.clear()
+
+    // Множество
+    println("MUTABLE SET")
+    val set = setOf("efasf", "fasf", "afs")
+    val mutableSet1 = mutableSetOf(1, 2, 3, 3, 3, 3, 3)
+    mutableSet1.forEach { println(it) }
+    val mutableSet2 = mutableSetOf(3, 4, 5)
+    println(mutableSet1.union(mutableSet2))
+    println(mutableSet1.intersect(mutableSet2))
+    println(mutableSet1.subtract(mutableSet2))
+
+    // Ассоциативный массив
+    val mutableMap = mutableMapOf("str" to 1, 3 to "Hi!", false to 12, false to 4)
+    mutableMap.forEach {
+        println("Key ${it.key} = value ${it.value}")
+    }
+    mutableMap[3] = 56
+
+    // Null безопастность
+    println("NULL")
+    val l = null
+    var str: String? = "Hello"
+    str = "Hi!"
+    println(str)
+    str = null
+    println(str)
+    println(str?.length)
+
+    val listNull = listOf("asfa", "asfsaf", null, "fsaf")
+    listNull.forEach {
+        println(it?.length)
+    }
+
+    // Try-catch
+    println("TRY-CATCH")
+    try {
+        listNull.forEach {
+            println(it!!.length)
+        }
+    } catch (e: Exception) {
+        println("Exception")
+    }
 }
